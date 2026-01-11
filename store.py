83acbeda0c1da5c9e303e1f1e0a24fdefc5bae4e
whacktor <lammelviktor2@gmail.com>
2026-01-11 15:17:21 +0100

    Finished and tested Store

--- store.py
+++ store.py
@@ -1,1 +1,47 @@
-#first commit
+import products
+
+
+class Store:
+    def __init__(self, products_list):
+        self.products = products_list
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def remove_product(self, product):
+        if product in self.products:
+            self.products.remove(product)
+
+    def get_total_quantity(self):
+        total = 0
+        for product in self.products:
+            total += product.get_quantity()
+        return total
+
+    def get_all_products(self):
+        active_products = []
+        for product in self.products:
+            if product.is_active():
+                active_products.append(product)
+        return active_products
+
+    def order(self, shopping_list):
+        total_price = 0.0
+        for product, quantity in shopping_list:
+            total_price += product.buy(quantity)
+        return total_price
+
+
+# ---------------- TEST / MAIN ----------------
+if __name__ == "__main__":
+    product_list = [
+        products.Product("MacBook Air M2", price=1450, quantity=100),
+        products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+        products.Product("Google Pixel 7", price=500, quantity=250),
+    ]
+
+    best_buy = Store(product_list)
+
+    active_products = best_buy.get_all_products()
+    print(best_buy.get_total_quantity())
+    print(best_buy.order([(active_products[0], 1), (active_products[1], 2)]))
